6eeb4a776eb5f918b9d29b9a0d870341fedd3ede
mahedihsharif <mahedi5061@gmail.com>
2025-07-05 12:22:54 +0600

    'update:vercel'

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://library-management-app-one-mu.vercel.app",
+    origin: [
+      "http://localhost:5173",
+      "https://library-management-app-one-mu.vercel.app",
+    ],
   })
 );
 
